8b91e810b82e197b14ca916e7e23813793a68bb0
AlexTair <alina.deniskova@gmail.com>
2025-07-05 01:42:23 +0300

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,41 @@
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+
+export const unstable_settings = {
+  initialRouteName: "(tabs)",
+};
+
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const [loaded, error] = useFonts({});
+
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <>
+      <StatusBar style="dark" />
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="list/[id]" options={{ headerShown: false }} />
+        <Stack.Screen name="add-item" options={{ presentation: "modal", headerShown: false }} />
+        <Stack.Screen name="group-settings" options={{ presentation: "modal", headerShown: false }} />
+      </Stack>
+    </>
+  );
+}
